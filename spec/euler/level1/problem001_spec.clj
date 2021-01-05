48880d112e7e3b839fb0d6d6ab3e693fd3b5bec4
zachcorrea <68715908+zachcorrea@users.noreply.github.com>
2021-01-05 12:56:22 -0500

    just getting started - coding

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -7,6 +7,20 @@
 
 (describe "Euler Problem #1"
 
+  (it "multiple of 3 or 5?"
+    (should= false (multiple-of-3-or-5? 0))
+	(should= false (multiple-of-3-or-5? 1))
+	(should= true (multiple-of-3-or-5? 3))
+	(should= true (multiple-of-3-or-5? 5))
+	(should= true (multiple-of-3-or-5? 6)))
+
+  (it "multiples-up-to n"
+    (should= [] (multiples-up-to 0))
+	(should= [3] (multiples-up-to 4))
+	(should= [3 5 6 9] (multiples-up-to 10))
+  )
+	
+	
   (it "Solves #1"
     (should= 23 (euler-1 10))
 	(should= -1 (euler-1 1000)))
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,18 @@
 (ns euler.level1.problem001)
 
+(defn multiple-of-3-or-5? [n]
+  (and 
+	(not= n 0)
+    (or
+		(= 0 (mod n 3))
+  		(= 0 (mod n 5))
+  )))
+  
+(defn multiples-up-to [n]
+	(filter multiple-of-3-or-5? (range n))
+
+	)
+  
 (defn euler-1 [n]
-  )
+  (apply + (multiples-up-to n)))
+  
